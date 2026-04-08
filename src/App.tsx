98048ad2aa20b1bd6e98af0dab9fe35d70aa1f2c
natnael <nsisay49@gmail.com>
2026-04-08 21:13:03 +0300

    color assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Faq from "./components/Faq"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+import Testimonials from "./components/Testimonials"
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <section className="h-screen p-4">
+        <div className="p-5 h-[98%] bg-muted-bg rounded-2xl">
+          <Header/>
+          <Hero />
+        </div>
+      </section>
+      <About />
+      <Testimonials />
+      <Faq />
+      <Contact />
+      <Footer />
+    </main>
   )
 }
 
---
+++ src/assets/colors.ts
@@ -0,0 +1,26 @@
+export const colors = {
+    primary: "#D4A857",
+    bluish: "#2F4A6D",
+    reddish: "#B8452E",
+    greenish: "#3E6B5A",
+
+    //light mode
+    mainBg: "#F7F5F2",
+    cardBg: "#FFFFFF",
+    mutedBg: "#EFEAE3",
+    textPriamry: "#111111",
+    textSecondary: "#6B6B6B",
+    textTertiary: "#E0E0E0",
+
+    //dark mode
+    darkMainBg: "#0F0F10",
+    darkCardBg: "#1A1A1C",
+    darkMutedBg: "#222225",
+    darkTextPrimary: "#FFFFFF",
+    darkTextSecondary: "#A1A1AA",
+
+    yellowAccent: "#E0B96A",
+    blueAccent: "#3A5F85",
+    redAccent: "#C6543C",
+
+}
