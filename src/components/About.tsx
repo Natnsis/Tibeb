d6434fad06177136cacde293badf3c86fd3fb738
natnael <nsisay49@gmail.com>
2026-04-09 15:57:09 +0300

    about page, param

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { HugeiconsIcon } from "@hugeicons/react";
+import { ArrowUpRight01Icon } from "@hugeicons/core-free-icons";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className="h-[40vh] mt-15">
+      <div className="grid grid-cols-3">
+        <div className="flex px-10 items-start col-span-1">
+          <p className="bg-muted-bg px-3 rounded-full">About us</p>
+        </div>
+        <div className="col-span-2 pr-15">
+          <p className="text-3xl border-b pb-10">
+            Wheather you're seeking a tranquil retreat, a cultural adventure or
+            an exhalirating outdoor experience, there is a perfect local escape
+            waiting for you.
+          </p>
+          <div className="py-10 flex justify-between">
+            <div className="border-r flex flex-col w-full p-5">
+              <p>EXPERT INTALLATION</p>
+              <h1>50+</h1>
+            </div>
+            <div className="border-r flex flex-col w-full p-5">
+              <p>SUPPORT</p>
+              <h1>24/7</h1>
+            </div>
+            <div className="flex flex-col w-full p-5">
+              <p>WARRANTY</p>
+              <h1>3+</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,11 @@
+import { HugeiconsIcon } from "@hugeicons/react";
+import { ArrowUpRight01Icon } from "@hugeicons/core-free-icons";
+
 const Hero = () => {
   return (
     <section className="flex flex-col flex-1">
       <div className="flex justify-center w-full h-full">
-        <div className="w-[45vw]">
+        <div className="w-[40vw]">
           <img src="/hero.png" alt="hero-img" className="w-full h-full" />
         </div>
       </div>
@@ -13,14 +16,15 @@ const Hero = () => {
             Discover Habesha themed ui components
           </p>
         </div>
-        <div className="w-[40vw] flex flex-col gap-2">
+        <div className="w-[40vw] flex flex-col gap-5">
           <p className="text-secondary">
             An accessible React component library inspired by the intricate
             patterns of Ethiopian Tibeb. Build modern web interfaces with a
             touch of heritage.
           </p>
-          <button className="bg-primary rounded-lg px-2 py-1 w-fit">
-            Get started
+          <button className="bg-primary rounded-lg px-2 py-1 w-fit flex items-center gap-2">
+            <p>Get started</p>
+            <HugeiconsIcon icon={ArrowUpRight01Icon} size={15} />
           </button>
         </div>
       </div>
