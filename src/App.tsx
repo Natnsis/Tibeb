56529cf16ee520b9ee4ee372e4b4f7f0ceb2c49d
natnael <nsisay49@gmail.com>
2026-04-19 21:18:00 +0300

    button variants

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,41 @@ import Button from "./components/Button";
 
 const App = () => {
   return (
-    <main className='flex flex-col'>
-      <h1>hello there</h1>
-      <div className='space-x-10'>
-        <Button className='text-primary'>hehe</Button>
-        <Button>hehe</Button>
+    <main className='grid grid-cols-4 gap-10 p-10'>
+      <div className='flex flex-col gap-10'>
+        <Button size='sm'>primary-sm</Button>
+        <Button size='md'>primary-md</Button>
+        <Button size='lg'>primary-lg</Button>
+      </div>
+
+      <div className='flex flex-col gap-10'>
+        <Button size='sm' variant='secondary'>secondary-sm</Button>
+        <Button size='md' variant='secondary'>secondary-md</Button>
+        <Button size='lg' variant='secondary'>secondary-lg</Button>
+      </div>
+
+      <div className='flex flex-col gap-10'>
+        <Button size='sm' variant='outline'>outline-sm</Button>
+        <Button size='md' variant='outline'>outline-md</Button>
+        <Button size='lg' variant='outline'>outline-lg</Button>
+      </div>
+
+      <div className='flex flex-col gap-10'>
+        <Button size='sm' variant='ghost'>ghost-sm</Button>
+        <Button size='md' variant='ghost'>ghost-md</Button>
+        <Button size='lg' variant='ghost'>ghost-lg</Button>
+      </div>
+
+      <div className='flex flex-col gap-10'>
+        <Button size='sm' variant='destructive'>destructive-sm</Button>
+        <Button size='md' variant='destructive'>destructive-md</Button>
+        <Button size='lg' variant='destructive'>ghost-lg</Button>
+      </div>
+
+      <div className='flex flex-col gap-10'>
+        <Button size='sm' variant='warning'>warning-sm</Button>
+        <Button size='md' variant='warning'>warning-md</Button>
+        <Button size='lg' variant='warning'>warning-lg</Button>
       </div>
   </main>
   )
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,34 +6,40 @@ const buttonVariants = cva(
   "inline-flex justify-center items-center", {
   variants: {
     variant: {
-      primary: '',
-      secondary: '',
-      destructive: '',
-      warning: ''
+      primary: 'bg-primary text-white cursor-pointer',
+      secondary: 'bg-blue-200',
+      outline: 'bg-zinc-100',
+      ghost: 'hover:bg-zinc-200',
+      destructive: 'bg-red-600 text-white',
+      warning: 'bg-yellow-400 text-black hover:bg-yellow-500'
     },
     size: {
-      default: 'h-12 w-fit',
+      sm: 'h-8 w-fit px-3 pb-1 rounded-sm text-sm',
+      md: 'h-10 w-fit px-4 pb-1 rounded-md text-md',
+      lg: 'h-12 w-fit px-6 pb-1 rounded-lg text-lg',
       icon: 'h-12 w-12'
     }
   },
-    defaultVariants:{
-      variant:'primary',
-      size:'default'
-    }
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md'
+  }
 }
 )
 
 
 export default function Button(props) {
-
-  const mergedProps = mergeProps(props,{
-    className:buttonVariants
+  const mergedProps = mergeProps(props, {
+    className: buttonVariants({
+      variant: props.variant,
+      size: props.size
+    })
   })
 
   const element = useRender({
     defaultTagName: 'button',
     render: props.render,
-    props:mergedProps
+    props: mergedProps
   });
 
   return element;
